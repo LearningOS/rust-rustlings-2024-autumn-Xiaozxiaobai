ac6d0035369029e62caef1d15da1b160180e1aee
xiaozxiaobai <xiaozxiaobai@gmail.com>
2024-10-08 21:16:31 +0800

    10.8

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,7 +14,6 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
@@ -39,6 +38,23 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        if scores.contains_key(&team_1_name) {
+            let mut ptr = scores.get_mut(&team_1_name);
+            ptr.unwrap().goals_scored += team_1_score;
+            let mut ptr = scores.get_mut(&team_1_name);
+            ptr.unwrap().goals_conceded += team_2_score;
+        }
+        else {
+            scores.insert(team_1_name,Team{goals_scored: team_1_score, goals_conceded:team_2_score});
+        }
+        if let Some(ptr) = scores.get_mut(&team_2_name) {
+            ptr.goals_scored += team_2_score;
+            ptr.goals_conceded += team_1_score;
+        }
+        else {
+            scores.insert(team_2_name,Team{goals_scored: team_2_score, goals_conceded:team_1_score});
+        }
+
     }
     scores
 }
